67594645702455c87d3ab4ea305a5e8170676682
rayzsyt <alihan290404@gmail.com>
2026-03-02 20:14:04 +0100

    Implemented code to download all necessary asset images

---
+++ vit-launcher/src/main/java/de/rayzs/vit/start/AssetPreparer.java
@@ -0,0 +1,271 @@
+package de.rayzs.vit.start;
+
+import de.rayzs.vit.api.VITAPI;
+import de.rayzs.vit.api.download.DownloadElement;
+import de.rayzs.vit.api.download.DownloadProcess;
+import de.rayzs.vit.api.file.FileDir;
+import de.rayzs.vit.api.image.DisplayImage;
+import de.rayzs.vit.api.items.Agent;
+import de.rayzs.vit.api.items.Tier;
+import de.rayzs.vit.api.request.Request;
+import de.rayzs.vit.api.request.RequestDest;
+import de.rayzs.vit.api.request.RequestMethod;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.util.*;
+
+public class AssetPreparer {
+
+    // Format for the images at valorant-api:
+    // maps,     id,     displayicon (small) / splash (normal)
+    // agent,    id,     displayicon (small)
+    private final String IMAGE_URL          = "https://media.valorant-api.com/%s/%s/%s.png";
+
+    private final String IMAGE_TARGET_MAPS  = "maps";
+    private final String IMAGE_TARGET_AGENTS = "agents";
+
+    private final String IMAGE_SIZE_BIG     = "splash";
+    private final String IMAGE_SIZE_SMALL   = "displayicon";
+
+
+    // These map images do not have a mini version of the image.
+    // Aka no mini map.
+    private final String[] SMALL_MAPS_IMAGE_BLACKLIST = new String[] {
+            // Tutorial? Or the Range? Not sure
+            "/Game/Maps/NPEV2/NPEV2",
+
+            // The range
+            "/Game/Maps/Poveglia/Range",
+            "/Game/Maps/PovegliaV2/RangeV2",
+
+            // Duel game mode
+            "/Game/Maps/Duel/Duel_1/Skirmish_A",
+            "/Game/Maps/Duel/Duel_2/Skirmish_B",
+            "/Game/Maps/Duel/Duel_3/Skirmish_C",
+    };
+
+
+
+    private final VITAPI api;
+
+    public AssetPreparer(VITAPI api) {
+        this.api = api;
+
+        final HttpClient client = Request.createClient();
+
+        loadWeapons(client);
+        loadAgents(client);
+        loadMaps(client);
+        loadTiers(client);
+
+        client.close();
+
+
+        Map<FileDir, HashSet<DownloadElement>> images = new HashMap<>();
+        for (final FileDir dir : FileDir.values()) {
+            for (final DisplayImage image : api.getImageProvider().getImages(dir)) {
+                images.computeIfAbsent(dir, k -> new HashSet<>())
+                        .add(image.getDownloadElement());
+            }
+        }
+
+        for (final FileDir dir : FileDir.values()) {
+            final DownloadProcess process = new DownloadProcess(
+                    dir,
+                    images.getOrDefault(
+                            dir,
+                            HashSet.newHashSet(0)
+                    ).toArray(new DownloadElement[0])
+            );
+
+            process.start(p -> {
+                try {
+                    System.out.write(("\rDownloading " + dir.getFolderName() + ": " + Math.round(p.getPercent()) + "%").getBytes());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
+    public void loadAgents(final HttpClient client) {
+        final JSONObject jsonObj = fetch(client, "agents");
+        final JSONArray agents = jsonObj.getJSONArray("data");
+
+        for (final Object agentObj : agents) {
+            final JSONObject gun = (JSONObject) agentObj;
+
+            final String id = gun.getString("uuid");
+            final String name = gun.getString("displayName");
+
+            final Agent agent = Agent.getAgentByName(name);
+
+            if (agent == null) {
+                throw new NullPointerException("Agent " + name + " not found!");
+            }
+
+            agent.updateAgentId(id);
+            api.getImageProvider().getAgents().putName(id, name);
+
+            api.getImageProvider().getAgents().putImage(
+                    id,
+                    IMAGE_URL.formatted(IMAGE_TARGET_AGENTS, id, IMAGE_SIZE_SMALL)
+            );
+        }
+    }
+
+    public void loadMaps(final HttpClient client) {
+        final JSONObject jsonObj = fetch(client, "maps");
+        final JSONArray maps = jsonObj.getJSONArray("data");
+
+        for (final Object mapObj : maps) {
+            final JSONObject map = (JSONObject) mapObj;
+
+            final String url = map.getString("mapUrl"); // Using for blacklist
+            final String name = map.getString("displayName");
+            final String id = map.getString("uuid");
+
+            api.getImageProvider().getMaps().putName(id, name);
+
+            api.getImageProvider().getMaps().putImage(id,
+                    IMAGE_URL.formatted(IMAGE_TARGET_MAPS, id, IMAGE_SIZE_BIG)
+            );
+
+
+            // Check if map actually has a mini image by checking
+            // if it's on the blacklist.
+            boolean ignore = false;
+            for (final String blacklist : SMALL_MAPS_IMAGE_BLACKLIST) {
+                if (url.equalsIgnoreCase(blacklist)) {
+                    ignore = true;
+                    break;
+                }
+            }
+
+            if (ignore) continue;
+
+            api.getImageProvider().getMaps().putMiniImage(id,
+                    IMAGE_URL.formatted(IMAGE_TARGET_MAPS, id, IMAGE_SIZE_SMALL)
+            );
+        }
+    }
+
+    public void loadTiers(final HttpClient client) {
+        final JSONObject jsonObj = fetch(client, "competitivetiers");
+
+        final JSONArray acts = jsonObj.getJSONArray("data");
+        final JSONObject act = acts.getJSONObject(acts.length() - 1);
+        final JSONArray tiers = act.getJSONArray("tiers");
+
+        for (final Object tierObj : tiers) {
+            final JSONObject rank = (JSONObject) tierObj;
+
+            final String id = String.valueOf(rank.getInt("tier"));
+
+            final String fullName = rank.getString("tierName");
+            final String[] fullNameParts = fullName.split(" ", 2);
+
+            final String rankName = fullNameParts[0];
+            final int tierNum = fullNameParts.length == 2
+                    ? Integer.parseInt(fullNameParts[1])
+                    : 0;
+
+            final Object smallIconObj = rank.get("smallIcon");
+            final Object largeIconObj = rank.get("largeIcon");
+
+            if (smallIconObj instanceof String smallIcon && largeIconObj instanceof String largeIcon) {
+                final String color = rank.getString("color");
+                final String name = rankName + (tierNum == 0 ? "" : " " + ("I").repeat(tierNum));
+                final Tier tier = Tier.getTierByName(name);
+
+                if (tier == null) {
+                    throw new NullPointerException("Tier " + name + " not found!");
+                }
+
+                tier.updateTierColor(color);
+                tier.updateTierId(id);
+
+                api.getImageProvider().getTiers().putName(id, name);
+
+                api.getImageProvider().getTiers().putImage(
+                        id,
+                        largeIcon
+                );
+
+                api.getImageProvider().getTiers().putMiniImage(
+                        id,
+                        smallIcon
+                );
+
+            }
+        }
+    }
+
+    public void loadWeapons(final HttpClient client) {
+        final JSONObject jsonObj = fetch(client, "weapons");
+        final JSONArray guns = jsonObj.getJSONArray("data");
+
+        for (final Object gunObj : guns) {
+            final JSONObject gun = (JSONObject) gunObj;
+
+            final String id = gun.getString("uuid");
+            final String name = gun.getString("displayName");
+            final String displayIcon = gun.getString("displayIcon");
+
+            api.getImageProvider().getWeaponSkins().putName(id, name);
+            api.getImageProvider().getWeaponSkins().putImage(id, displayIcon);
+
+            for (final Object skinObj : gun.getJSONArray("skins")) {
+                final JSONObject skin = (JSONObject) skinObj;
+
+                final String skinId = skin.getString("uuid");
+                final String skinName = skin.getString("displayName");
+                final Object displaySkinIconObj = skin.get("displayIcon");
+
+                if (skinName.equals(name)) {
+                    continue;
+                }
+
+                api.getImageProvider().getWeaponSkins().putName(skinId, skinName);
+
+                if (! (displaySkinIconObj instanceof String displaySkinIcon)) {
+                    final JSONArray chromas = skin.getJSONArray("chromas");
+                    final JSONObject firstChroma = (JSONObject) chromas.get(0);
+                    final Object displayChromaIconObj = firstChroma.get("displayIcon");
+
+                    if (! (displayChromaIconObj instanceof String displayChromaIcon)) {
+                        final JSONArray levels = skin.getJSONArray("levels");
+                        final JSONObject firstLevel = (JSONObject) levels.get(0);
+                        final String displayLevelIcon = firstLevel.getString("displayIcon");
+
+                        api.getImageProvider().getWeaponSkins().putImage(skinId, displayLevelIcon);
+                    } else {
+                        api.getImageProvider().getWeaponSkins().putImage(skinId, displayChromaIcon);
+                    }
+
+                } else {
+                    api.getImageProvider().getWeaponSkins().putImage(skinId, displaySkinIcon);
+                }
+
+            }
+        }
+    }
+
+    private JSONObject fetch(final HttpClient client, final String name) {
+        final Request request = Request.createRequest(
+                RequestMethod.GET,
+                RequestDest.API,
+                name
+        );
+
+        final Optional<String> body = request.sendAndGet(client);
+        if (body.isEmpty()) {
+            throw new NullPointerException("No body found! URL connection seems to have failed.");
+        }
+
+        return new JSONObject(body.get());
+    }
+}
