3184dfcc69197517512dcaac5397449b501f481f
rayzsyt <alihan290404@gmail.com>
2026-03-02 20:53:43 +0100

    Implement changes

--- vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
+++ vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
@@ -4,6 +4,8 @@ import de.rayzs.vit.api.VIT;
 import de.rayzs.vit.impl.VITAPIImpl;
 import de.rayzs.vit.start.AssetPreparer;
 
+import java.io.IOException;
+
 public class Bootstrap {
 
     public static void main(String[] args) {
@@ -11,7 +13,15 @@ public class Bootstrap {
         final VITAPIImpl api = new VITAPIImpl();
         VIT.set(api);
 
-        final AssetPreparer prep = new AssetPreparer(api);
+
+
+        final AssetPreparer prep = new AssetPreparer(api, process -> {
+            try {
+                System.out.write(("\r" + process.getPercent() + "%  ").getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
     }
 }
