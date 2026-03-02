ac5d7297bf676dfdcda2c5af6fbab3c4b5feab62
rayzsyt <alihan290404@gmail.com>
2026-03-02 19:41:39 +0100

    Implemented actual groundwork to work with

---
+++ vit-api/src/main/java/de/rayzs/vit/api/items/Tier.java
@@ -0,0 +1,144 @@
+package de.rayzs.vit.api.items;
+
+import de.rayzs.vit.api.VIT;
+import de.rayzs.vit.api.image.DisplayImage;
+
+public enum Tier {
+
+    // This one exists?
+    IRON_I              ("Iron I"),
+    IRON_II             ("Iron II"),
+    IRON_III            ("Iron III"),
+
+    // Skipped the rank hehe.
+    BRONZE_I            ("Bronze I"),
+    BRONZE_II           ("Bronze II"),
+    BRONZE_III          ("Bronze III"),
+
+    // Was there. Good old times.
+    SILVER_I            ("Silver I"),
+    SILVER_II           ("Silver II"),
+    SILVER_III          ("Silver III"),
+
+    // I'm still hard-stuck there ;-;
+    GOLD_I              ("Gold I"),
+    GOLD_II             ("Gold II"),
+    GOLD_III            ("Gold III"),
+
+    // Sweaters.
+    PLATINUM_I          ("Platinum I"),
+    PLATINUM_II         ("Platinum II"),
+    PLATINUM_III        ("Platinum III"),
+
+    // Bigger sweaters.
+    DIAMOND_I           ("Diamond I"),
+    DIAMOND_II          ("Diamond II"),
+    DIAMOND_III         ("Diamond III"),
+
+    // HOW?
+    ASCENDANT_I         ("Ascendant I"),
+    ASCENDANT_II        ("Ascendant II"),
+    ASCENDANT_III       ("Ascendant III"),
+
+    // ARE YOU EVEN EMPLOYED?
+    IMMORTAL_I          ("Immortal I"),
+    IMMORTAL_II         ("Immortal II"),
+    IMMORTAL_III        ("Immortal III"),
+
+    // I think this is you job, right?
+    RADIANT             ("Radiant");
+
+
+    /**
+     * Yes I know. I'm very professional. xd
+     */
+
+
+    private String tierId;
+    private final String tierName;
+
+    Tier(final String tierName) {
+        this.tierName = tierName;
+    }
+
+    /**
+     * Returns the agent name.
+     *
+     * @return Agent name.
+     */
+    public String getTierName() {
+        return this.tierName;
+    }
+
+    /**
+     * Update tier id. Should only be called once
+     * during runtime to set the agent id fetched from
+     * the valorant-api.
+     *
+     * @param tierId Tier id.
+     */
+    public void updateTierId(final String tierId) {
+        if (this.tierId != null) {
+            throw new IllegalStateException("Tier ID is already set!");
+        }
+
+        this.tierId = tierId;
+    }
+
+    /**
+     * Get the tier id. This one needs to be set first after
+     * fetching the agent uuid from the valorant-api.
+     * It can then be used to easier navigate through
+     * each agent's images and other information.
+     *
+     * @return Tier id.
+     */
+    public String getTierId() {
+        return this.tierId;
+    }
+
+    /**
+     * Returns the DisplayImage of the tier.
+     *
+     * @return DisplayImage.
+     */
+    public DisplayImage getImage() {
+        return VIT.get().getImageProvider().getTiers().getImage(this.tierId);
+    }
+
+    /**
+     * Iterates through all tiers and compare their names
+     * with the provided input. Returns the agent if it finds a match,
+     * otherwise returns null.
+     *
+     * @param tierName Tier name. Letter casing does not matter!
+     * @return Returns matched agent or null.
+     */
+    public static Tier getTierByName(final String tierName) {
+        for (final Tier tier : Tier.values()) {
+            if (tier.getTierName().equalsIgnoreCase(tierName)) {
+                return tier;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Iterates through all tiers and compare their ids
+     * with the provided input. Returns the agent if it finds a match,
+     * otherwise returns null.
+     *
+     * @param tierId Tier id. Letter casing does not matter!
+     * @return Returns matched agent or null.
+     */
+    public static Tier getTierById(final String tierId) {
+        for (final Tier tier : Tier.values()) {
+            if (tier.getTierId().equalsIgnoreCase(tierId)) {
+                return tier;
+            }
+        }
+
+        return null;
+    }
+}
