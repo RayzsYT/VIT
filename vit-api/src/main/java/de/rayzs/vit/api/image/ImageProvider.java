12e2ad22dea9c6b27dcfadfe9c210e8b9f885389
rayzsyt <alihan290404@gmail.com>
2026-03-02 02:03:37 +0100

    Implement code structure

--- vit-api/src/main/java/de/rayzs/vit/api/image/ImageProvider.java
+++ vit-api/src/main/java/de/rayzs/vit/api/image/ImageProvider.java
@@ -1,6 +1,31 @@
 package de.rayzs.vit.api.image;
 
-public class ImageProvider {
+import de.rayzs.vit.api.file.FileDir;
+import java.util.Collection;
 
+public interface ImageProvider {
 
+    /**
+     * Get an already existing or create
+     * a new DisplayImage object.
+     *
+     * @param url URL where to download the image.
+     * @param dir FileDir where to store the file.
+     * @param fileName File name.
+     * @return DisplayImage.
+     */
+    DisplayImage getOrCreateImage(
+            final String url,
+            final FileDir dir,
+            final String fileName
+    );
+
+    /**
+     * Get all DisplayImages
+     * from the FileDir.
+     *
+     * @param dir FileDir.
+     * @return Collection of all DisplayImages.
+     */
+    Collection<DisplayImage> getImages(final FileDir dir);
 }
