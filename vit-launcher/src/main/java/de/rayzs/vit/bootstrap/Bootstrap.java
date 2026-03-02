888a1cf6893b2eba7724b0de0e6381747f33551e
rayzsyt <alihan290404@gmail.com>
2026-03-02 05:00:08 +0100

    Add

--- vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
+++ vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
@@ -1,8 +1,17 @@
 package de.rayzs.vit.bootstrap;
 
+import de.rayzs.vit.api.VIT;
+import de.rayzs.vit.impl.VITAPIImpl;
+import de.rayzs.vit.start.VITStart;
+
 public class Bootstrap {
 
     public static void main(String[] args) {
+
+        final VITAPIImpl api = new VITAPIImpl();
+        VIT.set(api);
+
+        final VITStart start = new VITStart(api);
 
     }
 }
