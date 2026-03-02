706fcb713d037692e8b4d00e96a20a9cdfbc9901
rayzsyt <alihan290404@gmail.com>
2026-03-02 02:21:41 +0100

    Added ImageProvider method

--- vit-api/src/main/java/de/rayzs/vit/api/VITAPI.java
+++ vit-api/src/main/java/de/rayzs/vit/api/VITAPI.java
@@ -1,4 +1,14 @@
 package de.rayzs.vit.api;
 
+import de.rayzs.vit.api.image.ImageProvider;
+
 public interface VITAPI {
+
+    /**
+     * Get the ImageProvider to create
+     * or get DisplayImages.
+     * 
+     * @return ImageProvider.
+     */
+    ImageProvider getImageProvider();
 }
