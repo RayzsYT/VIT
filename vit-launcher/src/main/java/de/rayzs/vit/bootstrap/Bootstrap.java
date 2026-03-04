6bf2800d05c9f3f780203ace9dc98536c5b21f34
rayzsyt <alihan290404@gmail.com>
2026-03-04 02:57:57 +0100

    Temporary auto update of gui for testing

--- vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
+++ vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
@@ -1,8 +1,13 @@
 package de.rayzs.vit.bootstrap;
 
 import de.rayzs.vit.api.VIT;
+import de.rayzs.vit.api.gui.MainGUI;
 import de.rayzs.vit.impl.VITAPIImpl;
-import de.rayzs.vit.processes.AssetPreparer;
+import de.rayzs.vit.processes.screen.InactiveScreen;
+import de.rayzs.vit.processes.screen.LoadingScreen;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Bootstrap {
 
@@ -11,8 +16,30 @@ public class Bootstrap {
         final VITAPIImpl api = new VITAPIImpl();
         VIT.set(api);
 
-        final AssetPreparer prep = new AssetPreparer(api);
+
+        //final AssetPreparer prep = new AssetPreparer(api);
 
 
+        final MainGUI gui = new MainGUI("Waiting");
+        gui.setVisible(true);
+
+        final InactiveScreen inactiveScreen = new InactiveScreen();
+        final LoadingScreen loadingScreen = new LoadingScreen();
+        inactiveScreen.load(api, gui);
+
+        TimerTask task = new TimerTask() {
+            boolean b = true;
+
+            public void run() {
+                b = !b;
+
+                System.out.println(b);
+
+                if (b) inactiveScreen.load(api, gui);
+                else loadingScreen.load(api, gui);
+            }
+        };
+
+        new Timer().scheduleAtFixedRate(task, 5000, 5000);
     }
 }
