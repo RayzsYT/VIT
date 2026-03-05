a7ed1d2d7bbebd008a4cc59c895d2bb8121b37bd
rayzsyt <alihan290404@gmail.com>
2026-03-05 03:56:56 +0100

    Add bandit weapon

--- vit-api/src/main/java/de/rayzs/vit/api/objects/items/Weapon.java
+++ vit-api/src/main/java/de/rayzs/vit/api/objects/items/Weapon.java
@@ -10,6 +10,7 @@ public enum Weapon {
 
     // Short-handed weapons.
     CLASSIC     ("Classic"),
+    BANDIT      ("Bandit"),
     GHOST       ("Ghost"),
     FRENZY      ("Frenzy"),
     SHERIFF     ("Sheriff"),
