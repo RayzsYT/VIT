6f3d531a1ab2ff077a424c5c62c2a01a895fafac
rayzsyt <alihan290404@gmail.com>
2026-03-05 02:55:31 +0100

    Implement loop handler

--- vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
+++ vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
@@ -3,10 +3,8 @@ package de.rayzs.vit.bootstrap;
 import de.rayzs.vit.api.VIT;
 import de.rayzs.vit.api.gui.MainGUI;
 import de.rayzs.vit.impl.VITAPIImpl;
+import de.rayzs.vit.processes.loop.LoopHandler;
 import de.rayzs.vit.processes.prepare.AssetPreparer;
-import de.rayzs.vit.processes.screen.InactiveScreen;
-import de.rayzs.vit.processes.screen.LiveScreen;
-import de.rayzs.vit.processes.screen.LoadingScreen;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -25,25 +23,14 @@ public class Bootstrap {
         final MainGUI gui = new MainGUI("Waiting");
         gui.setVisible(true);
 
-        final InactiveScreen inactiveScreen = new InactiveScreen();
-        final LoadingScreen loadingScreen = new LoadingScreen();
-        final LiveScreen liveScreen = new LiveScreen();
-        inactiveScreen.load(api, gui);
+        final LoopHandler loop = new LoopHandler(api, gui);
 
         TimerTask task = new TimerTask() {
-            int i = 1;
-
             public void run() {
-                i = (++i % 3);
-
-                switch (i) {
-                    case 0: inactiveScreen.load(api, gui); break;
-                    case 1: loadingScreen.load(api, gui); break;
-                    case 2: liveScreen.load(api, gui); break;
-                }
+                loop.handle();
             }
         };
 
-        new Timer().scheduleAtFixedRate(task, 5000, 3000);
+        new Timer().scheduleAtFixedRate(task, 0, 3000);
     }
 }
