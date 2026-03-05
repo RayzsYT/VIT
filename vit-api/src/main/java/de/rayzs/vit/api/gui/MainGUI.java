2baac6501cbdea892dd9a88fd720c91197816b48
rayzsyt <alihan290404@gmail.com>
2026-03-05 04:06:40 +0100

    Implement version

--- vit-api/src/main/java/de/rayzs/vit/api/gui/MainGUI.java
+++ vit-api/src/main/java/de/rayzs/vit/api/gui/MainGUI.java
@@ -1,5 +1,8 @@
 package de.rayzs.vit.api.gui;
 
+import de.rayzs.vit.api.VIT;
+import de.rayzs.vit.api.VITAPI;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,7 +14,7 @@ public class MainGUI extends GUI {
             "<html><div style='width:800px; text-align:center; color:rgba(255,255,255,0.2);'>",
             "VIT isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties.<br>",
             "Riot Games, and all associated properties are trademarks or registered trademarks of Riot Games, Inc.",
-            "<br><br>Version v",
+            "<br><br>Version v%s",
             "</div></html>"
     });
 
@@ -20,7 +23,7 @@ public class MainGUI extends GUI {
     private JPanel contentPane;
 
 
-    public MainGUI(String title) {
+    public MainGUI(final String title) {
         super(title, 1000, 900);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,7 +34,7 @@ public class MainGUI extends GUI {
         this.disclaimerPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         this.disclaimerPanel.setBackground(GUI.Colors.BACKGROUND.get());
 
-        final JLabel disclaimerLabel = new JLabel(DISCLAIMER);
+        final JLabel disclaimerLabel = new JLabel(DISCLAIMER.formatted(VIT.get().getVersion()));
         disclaimerLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 10));
         disclaimerPanel.add(disclaimerLabel);
 
