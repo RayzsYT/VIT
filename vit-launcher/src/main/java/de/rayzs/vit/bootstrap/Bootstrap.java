1a9417b6279ea182d890076dd351669e7117fade
rayzsyt <alihan290404@gmail.com>
2026-03-05 04:22:13 +0100

    Implement OutputLogger

--- vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
+++ vit-launcher/src/main/java/de/rayzs/vit/bootstrap/Bootstrap.java
@@ -14,6 +14,8 @@ public class Bootstrap {
     public static void main(String[] args) {
 
         try {
+            OutputLogger.initialize();
+
             start();
         } catch (Exception exception) {
             exception.printStackTrace();
