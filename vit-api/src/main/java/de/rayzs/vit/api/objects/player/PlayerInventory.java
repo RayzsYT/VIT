9078b2f2da5c9d3c1ef177160f0f32bda657ef90
rayzsyt <alihan290404@gmail.com>
2026-03-05 04:03:22 +0100

    Added getWeaponSkinName() method and improve how default weapons are set

--- vit-api/src/main/java/de/rayzs/vit/api/objects/player/PlayerInventory.java
+++ vit-api/src/main/java/de/rayzs/vit/api/objects/player/PlayerInventory.java
@@ -10,17 +10,22 @@ import java.util.Map;
 
 public class PlayerInventory {
 
+    private final VITAPI api;
     private final Map<Weapon, DisplayImage> weaponSkins = new HashMap<>();
+    private final Map<Weapon, String> weaponSkinNames = new HashMap<>();
 
     public PlayerInventory(final Map<Weapon, String> weaponIds) {
-        final VITAPI api = VIT.get();
+        this.api = VIT.get();
 
         for (Map.Entry<Weapon, String> entry : weaponIds.entrySet()) {
             final Weapon weapon = entry.getKey();
             final String skinId = entry.getValue();
 
+            final String skinName = api.getImageProvider().getWeaponSkins().getName(skinId);
+
             final DisplayImage image = api.getImageProvider().getWeaponSkins().getImage(skinId);
             this.weaponSkins.put(weapon, image);
+            this.weaponSkinNames.put(weapon, skinName);
         }
     }
 
@@ -33,6 +38,31 @@ public class PlayerInventory {
      * @return DisplayImage of the weapon skin.
      */
     public DisplayImage getWeaponSkin(final Weapon weapon) {
-        return weaponSkins.getOrDefault(weapon, weapon.getDefaultSkin());
+        final DisplayImage skin = this.weaponSkins.get(weapon);
+
+        if (skin == null) {
+            final DisplayImage defaultSkin = weapon.getDefaultSkin();
+
+            this.weaponSkins.put(weapon, defaultSkin);
+            return defaultSkin;
+        }
+
+        return skin;
+    }
+
+    public String getWeaponSkinName(final Weapon weapon) {
+        final String skinName = this.weaponSkinNames.get(weapon);
+
+        if (skinName == null) {
+            final String defaultSkinName = api
+                    .getImageProvider()
+                    .getWeaponSkins()
+                    .getName(weapon.getDefaultSkinId());
+
+            this.weaponSkinNames.put(weapon, defaultSkinName);
+            return defaultSkinName;
+        }
+
+        return skinName;
     }
 }
