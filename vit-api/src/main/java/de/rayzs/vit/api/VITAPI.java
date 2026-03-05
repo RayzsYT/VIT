64a1be85769681909e5302c4f1e6fa66f4cd7d89
rayzsyt <alihan290404@gmail.com>
2026-03-05 03:04:48 +0100

    Add version

--- vit-api/src/main/java/de/rayzs/vit/api/VITAPI.java
+++ vit-api/src/main/java/de/rayzs/vit/api/VITAPI.java
@@ -4,6 +4,8 @@ import de.rayzs.vit.api.image.ImageProvider;
 
 public interface VITAPI {
 
+    default String getVersion() { return "0.0.1"; }
+
     /**
      * Get the ImageProvider to create
      * or get DisplayImages.
