2a3c4332d4427ed1d44e8b3baa55cdaa0f1a446a
rayzsyt <alihan290404@gmail.com>
2026-03-06 23:50:54 +0100

    Add GameState

--- vit-api/src/main/java/de/rayzs/vit/api/objects/game/Game.java
+++ vit-api/src/main/java/de/rayzs/vit/api/objects/game/Game.java
@@ -5,16 +5,19 @@ import de.rayzs.vit.api.objects.player.Player;
 public class Game {
 
     private final Player self;
+    private final GameState gameState;
 
     private final Player[] players;
     private final String mapId;
 
     public Game(
             final Player self,
+            final GameState gameState,
             final Player[] players,
             final String mapId
     ) {
         this.self = self;
+        this.gameState = gameState;
         this.players = players;
         this.mapId = mapId;
     }
@@ -47,5 +50,16 @@ public class Game {
      */
     public Player[] getPlayers() {
         return this.players;
+    }
+
+    /**
+     * Get the current state of the game,
+     * if the client is in the lobby/agent selection,
+     * or already inside the match.
+     *
+     * @return State of the game.
+     */
+    public GameState getGameState() {
+        return gameState;
     }
 }
