425120b7fbe03dead7cebfdd7edf44984905d5f8
rayzsyt <alihan290404@gmail.com>
2026-03-06 04:31:07 +0100

    Implemented more code hehe

--- vit-launcher/src/main/java/de/rayzs/vit/processes/loop/LoopHandler.java
+++ vit-launcher/src/main/java/de/rayzs/vit/processes/loop/LoopHandler.java
@@ -2,11 +2,37 @@ package de.rayzs.vit.processes.loop;
 
 import de.rayzs.vit.api.VITAPI;
 import de.rayzs.vit.api.gui.MainGUI;
+import de.rayzs.vit.api.request.Request;
 import de.rayzs.vit.processes.screen.InactiveScreen;
 import de.rayzs.vit.processes.screen.LiveScreen;
 import de.rayzs.vit.processes.screen.LoadingScreen;
 
 public class LoopHandler {
+
+    private enum State {
+        INACTIVE, RUNNING, WAITING, LOADING, LIVE;
+
+        public boolean isInactive() {
+            return this == INACTIVE;
+        }
+
+        public boolean isRunning() {
+            return this == RUNNING;
+        }
+
+        public boolean isWaiting() {
+            return this == WAITING;
+        }
+
+        public boolean isLoading() {
+            return this == LOADING;
+        }
+
+        public boolean isLive() {
+            return this == LIVE;
+        }
+    }
+
 
     private final VITAPI api;
     private final MainGUI gui;
@@ -16,6 +42,8 @@ public class LoopHandler {
     private final LoadingScreen loadingScreen = new LoadingScreen();
     private final LiveScreen liveScreen = new LiveScreen();
 
+    private State state = State.INACTIVE;
+
 
     public LoopHandler(final VITAPI api, final MainGUI gui) {
         this.api = api;
@@ -23,16 +51,51 @@ public class LoopHandler {
 
         gui.setAlwaysOnTop(true);
         gui.setAlwaysOnTop(false);
-
-        if (api.getSession().isOpen()) {
-            this.loadingScreen.load(api, gui);
-        } else {
-            this.inactiveScreen.load(api, gui);
-        }
-
     }
 
     public void handle() {
 
+        if (!state.isInactive() && !this.api.getSession().isOpen()) {
+            state = State.INACTIVE;
+
+            Request.unsetAuthToken();
+            Request.unsetHeaders();
+
+            inactiveScreen.load(this.api, this.gui);
+            return;
+        }
+
+        if (state.isInactive() && this.api.getSession().isOpen()) {
+            state = State.WAITING;
+
+            loadingScreen.resetText();
+            api.getSession().initialize();
+
+            loadingScreen.load(this.api, this.gui);
+            return;
+        }
+
+
+        final boolean insideMatch = this.api.getSession().insideMatch();
+        final boolean preparing = state.isLive() || state.isLoading();
+
+        if (!preparing && insideMatch) {
+            state = State.LOADING;
+
+            loadLiveScreen();
+            return;
+        }
+
+
+        if (preparing && !insideMatch) {
+            state = State.WAITING;
+
+            loadingScreen.resetText();
+            loadingScreen.load(this.api, this.gui);
+        }
+    }
+
+    private void loadLiveScreen() {
+        // Create game object...
     }
 }
