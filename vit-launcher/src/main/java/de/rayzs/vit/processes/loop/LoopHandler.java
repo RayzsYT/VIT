559649f2e8ef1378ab1eb08fd56bb546c934c0ef
rayzsyt <alihan290404@gmail.com>
2026-03-07 05:31:31 +0100

    Improved logic

--- vit-launcher/src/main/java/de/rayzs/vit/processes/loop/LoopHandler.java
+++ vit-launcher/src/main/java/de/rayzs/vit/processes/loop/LoopHandler.java
@@ -8,33 +8,9 @@ import de.rayzs.vit.api.request.Request;
 import de.rayzs.vit.processes.screen.InactiveScreen;
 import de.rayzs.vit.processes.screen.LiveScreen;
 import de.rayzs.vit.processes.screen.LoadingScreen;
+import de.rayzs.vit.processes.screen.LobbyScreen;
 
 public class LoopHandler {
-
-    private enum State {
-        INACTIVE, RUNNING, WAITING, LOADING, LIVE;
-
-        public boolean isInactive() {
-            return this == INACTIVE;
-        }
-
-        public boolean isRunning() {
-            return this == RUNNING;
-        }
-
-        public boolean isWaiting() {
-            return this == WAITING;
-        }
-
-        public boolean isLoading() {
-            return this == LOADING;
-        }
-
-        public boolean isLive() {
-            return this == LIVE;
-        }
-    }
-
 
     private final VITAPI api;
     private final MainGUI gui;
@@ -42,9 +18,10 @@ public class LoopHandler {
 
     private final InactiveScreen inactiveScreen = new InactiveScreen();
     private final LoadingScreen loadingScreen = new LoadingScreen();
+    private final LobbyScreen lobbyScreen = new LobbyScreen();
     private final LiveScreen liveScreen = new LiveScreen();
 
-    private State state = State.INACTIVE;
+    private SessionState priorState;
 
 
     public LoopHandler(final VITAPI api, final MainGUI gui) {
@@ -57,62 +34,63 @@ public class LoopHandler {
         inactiveScreen.load(api, gui);
     }
 
+
     public void handle() {
 
         if (!Request.areHeadersSet()) {
-            state = State.INACTIVE;
-
             System.out.println("Waiting 'til VALORANT is enabled");
             api.getSession().initialize();
             return;
         }
 
-
         final SessionState sessionState = api.getSession().getSessionState();
 
-
-        if (!state.isInactive() && !sessionState.isValorantStarted()) {
-            System.out.println("Set state to inactive");
-            state = State.INACTIVE;
-
-            Request.unsetHeaders();
-
-            inactiveScreen.load(api, gui);
-            return;
-        }
-
-        if (state.isInactive() && sessionState.isValorantStarted()) {
-            System.out.println("Detected game. Set state to waiting");
-            state = State.WAITING;
-
-            loadingScreen.resetText();
-
-            loadingScreen.load(this.api, this.gui);
+        if (priorState == sessionState) {
             return;
         }
 
 
-        final boolean preparing = state.isLive() || state.isLoading();
+        priorState = sessionState;
+        loadingScreen.resetText();
 
-        if (!preparing && sessionState.isInsideMatch()) {
-            System.out.println("Found live match");
-            state = State.LOADING;
+        System.out.println("New state detected: " + sessionState.name());
 
-            loadLiveScreen();
-            return;
-        }
+        switch (sessionState) {
 
+            case VALORANT_NOT_OPEN -> {
+                inactiveScreen.load(api, gui);
+            }
 
-        if (preparing && !sessionState.isInsideMatch()) {
-            state = State.WAITING;
+            case IN_MENU -> {
+                loadingScreen.load(api, gui);
+            }
 
-            loadingScreen.resetText();
-            loadingScreen.load(this.api, this.gui);
+            case IN_LOBBY -> {
+                loadLobbyScreen();
+            }
+
+            case IN_GAME -> {
+                loadLiveScreen();
+            }
         }
     }
 
+    private void loadLobbyScreen() {
+        final Game game = api.getSession().constructGame(priorState, n -> {
+            loadingScreen.updateText("Loaded " + n + " players...");
+        });
+
+        if (game == null) {
+            return;
+        }
+
+        api.setGame(game);
+
+        lobbyScreen.load(api, gui);
+    }
+
     private void loadLiveScreen() {
-        final Game game = api.getSession().constructGame(n -> {
+        final Game game = api.getSession().constructGame(priorState, n -> {
             loadingScreen.updateText("Loaded " + n + " players...");
         });
 
