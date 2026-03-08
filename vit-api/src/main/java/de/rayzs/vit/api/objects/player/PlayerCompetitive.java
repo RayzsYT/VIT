f6431b0ba0a168c35963cca7fd10469b92fca35b
rayzsyt <alihan290404@gmail.com>
2026-03-08 04:43:44 +0100

    Applied renamed class

--- vit-api/src/main/java/de/rayzs/vit/api/objects/player/PlayerCompetitive.java
+++ vit-api/src/main/java/de/rayzs/vit/api/objects/player/PlayerCompetitive.java
@@ -2,13 +2,13 @@ package de.rayzs.vit.api.objects.player;
 
 import de.rayzs.vit.api.objects.items.Tier;
 import de.rayzs.vit.api.objects.player.competitive.CompRequirements;
-import de.rayzs.vit.api.objects.player.competitive.CompMatchResult;
+import de.rayzs.vit.api.objects.player.competitive.MatchResult;
 import de.rayzs.vit.api.objects.player.competitive.SeasonTiers;
 
 public record PlayerCompetitive(
         Tier currentTier,
         int rr,
         SeasonTiers seasonTiers,
-        CompMatchResult latestMatch,
+        MatchResult latestMatch,
         CompRequirements compRequirements
 ) { }
