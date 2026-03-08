d4824c85c53355e8af8715e79e8ea705003a7158
rayzsyt <alihan290404@gmail.com>
2026-03-08 04:42:59 +0100

    Add

---
+++ vit-api/src/main/java/de/rayzs/vit/api/objects/player/competitive/MatchStats.java
@@ -0,0 +1,14 @@
+package de.rayzs.vit.api.objects.player.competitive;
+
+/**
+ * What the player gained from that match.
+ *
+ * @param matchId ID of the match.
+ * @param mapId ID of map played there.
+ * @param headshots Headshots.
+ */
+public record MatchStats(
+        int matchId,            // ID how the match.
+        String mapId,           // ID of map being played on.
+        int headshots           // How much RR lost/gained after this match.
+) { }
