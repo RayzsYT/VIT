8c1630c136f641e5c4aeb8b8864c01b225d29e13
rayzsyt <alihan290404@gmail.com>
2026-03-08 16:18:01 +0100

    Removed overridden methods

--- vit-api/src/main/java/de/rayzs/vit/api/objects/game/Game.java
+++ vit-api/src/main/java/de/rayzs/vit/api/objects/game/Game.java
@@ -1,57 +1,12 @@
 package de.rayzs.vit.api.objects.game;
 
 import de.rayzs.vit.api.objects.player.Player;
+import de.rayzs.vit.api.objects.session.SessionState;
 
 public record Game(
-        Player self,
-        GameState gameState,
-        Player[] players,
-        String mapId,
-        String server
-) {
-
-
-    /**
-     * Get the id of the map
-     * being played on.
-     *
-     * @return Id of the map being played on.
-     */
-    @Override
-    public String mapId() {
-        return this.mapId;
-    }
-
-    /**
-     * Get the player who is
-     * using the program.
-     *
-     * @return Player.
-     */
-    @Override
-    public Player self() {
-        return this.self;
-    }
-
-    /**
-     * Get an array of all players.
-     *
-     * @return Array of all players.
-     */
-    @Override
-    public Player[] players() {
-        return this.players;
-    }
-
-    /**
-     * Get the current state of the game,
-     * if the client is in the lobby/agent selection,
-     * or already inside the match.
-     *
-     * @return State of the game.
-     */
-    @Override
-    public GameState gameState() {
-        return gameState;
-    }
-}
+        Player self,            // Self player
+        SessionState state,     // State
+        Player[] players,       // Players
+        String mapId,           // ID of map
+        String server           // Connected server
+) { }
