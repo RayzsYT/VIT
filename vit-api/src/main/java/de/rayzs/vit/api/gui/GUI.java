d1140751379c90b4ddfb787d9ea46d60036b9d7a
rayzsyt <alihan290404@gmail.com>
2026-03-09 21:40:55 +0100

    New Colors

--- vit-api/src/main/java/de/rayzs/vit/api/gui/GUI.java
+++ vit-api/src/main/java/de/rayzs/vit/api/gui/GUI.java
@@ -28,61 +28,64 @@ public class GUI extends JFrame {
 
     public enum Colors {
 
-        BACKGROUND                          (41, 41, 61),
-        BORDER                              (64, 64, 64),
+        BACKGROUND                                  (41, 41, 61),
+        BORDER                                      (64, 64, 64),
 
-        TOOLTIP_FOREGROUND                  (Color.WHITE),
-        TOOLTIP_BACKGROUND                  (36, 36, 53),
+        TOOLTIP_FOREGROUND                          (Color.WHITE),
+        TOOLTIP_BACKGROUND                          (36, 36, 53),
 
-        TEXT_FOREGROUND                     (Color.WHITE),
-        SELECTION_BACKGROUND                (Color.WHITE),
+        TEXT_FOREGROUND                             (Color.WHITE),
+        SELECTION_BACKGROUND                        (Color.WHITE),
 
-        STATS_TEXT_FOREGROUND               (204, 204, 204),
+        STATS_TEXT_FOREGROUND                       (204, 204, 204),
 
-        PROGRESS_BAR_SELECTED_TEXT          (74, 8, 12),
-        PROGRESS_BAR_FOREGROUND             (236, 97, 108),
-        PROGRESS_BAR_BACKGROUND             (BACKGROUND),
-
-
-        MAIN_CONTROL_BUTTON                 (206, 62, 62),
-
-        LOBBY_ROLE_ATTACKING                (248, 155, 155),
-        LOBBY_ROLE_DEFENDING                (161, 196, 255),
-
-        CONTROL_SCROLLBAR                   (206, 62, 62),
-        CONTROL_SCROLLBAR_HOVER             (218, 108, 108),
-
-        CONTROL_BUTTON_BACKGROUND           (206, 62, 62),
-        CONTROL_BUTTON_BACKGROUND_HOVER     (129, 27, 27),
-        CONTROL_BUTTON_BACKGROUND_PRESSED   (126, 2, 2),
-        CONTROL_BUTTON_BACKGROUND_RELEASED  (CONTROL_BUTTON_BACKGROUND),
-
-        PLAYER_MATCH_WON                    (209, 254, 200),
-        PLAYER_MATCH_LOST                   (248, 170, 170),
-
-        PLAYER_SCROLLBAR                    (67, 67, 99),
-
-        PLAYER_BUTTON_BACKGROUND            (206, 62, 62),
-        PLAYER_BUTTON_BACKGROUND_HOVER      (129, 27, 27),
-        PLAYER_BUTTON_BACKGROUND_PRESSED    (126, 2, 2),
-        PLAYER_BUTTON_BACKGROUND_RELEASED   (PLAYER_BUTTON_BACKGROUND),
-
-        BANNER_ATTACKER                     (206, 62, 62),
-        BANNER_DEFENDER                     (59, 110, 245),
-        BANNER_BACKGROUND                   (47, 47, 67),
-        BANNER_BORDER                       (0, 0, 0, 120),
+        PROGRESS_BAR_SELECTED_TEXT                  (74, 8, 12),
+        PROGRESS_BAR_FOREGROUND                     (236, 97, 108),
+        PROGRESS_BAR_BACKGROUND                     (BACKGROUND),
 
 
-        BUTTON_OPTION_HOVER                 (255, 93, 104),
-        BUTTON_OPTION_PRESSED               (80, 80, 96),
-        BUTTON_OPTION_RELEASED              (64, 64, 96),
-        BUTTON_OPTION_BACKGROUND            (64, 64, 96),
+        MAIN_CONTROL_BUTTON                         (206, 62, 62),
+
+        LOBBY_ROLE_ATTACKING                        (248, 155, 155),
+        LOBBY_ROLE_DEFENDING                        (161, 196, 255),
+
+        CONTROL_COMBOBOX_ARROW                      (Color.BLACK),
+        CONTROL_COMBOBOX_BACKGROUND                 (206, 62, 62),
+        CONTROL_COMBOBOX_HOVER                      (218, 108, 108),
+        CONTROL_COMBOBOX_SCROLL_BACKGROUND          (new Color(206, 62, 62)),
+        CONTROL_COMBOBOX_SCROLL_FOREGROUND          (new Color(105, 30, 30)),
+
+        CONTROL_BUTTON_BACKGROUND                   (206, 62, 62),
+        CONTROL_BUTTON_BACKGROUND_HOVER             (129, 27, 27),
+        CONTROL_BUTTON_BACKGROUND_PRESSED           (126, 2, 2),
+        CONTROL_BUTTON_BACKGROUND_RELEASED          (CONTROL_BUTTON_BACKGROUND),
+
+        PLAYER_MATCH_WON                            (209, 254, 200),
+        PLAYER_MATCH_LOST                           (248, 170, 170),
+
+        PLAYER_SCROLLBAR                            (67, 67, 99),
+
+        PLAYER_BUTTON_BACKGROUND                    (206, 62, 62),
+        PLAYER_BUTTON_BACKGROUND_HOVER              (129, 27, 27),
+        PLAYER_BUTTON_BACKGROUND_PRESSED            (126, 2, 2),
+        PLAYER_BUTTON_BACKGROUND_RELEASED           (PLAYER_BUTTON_BACKGROUND),
+
+        BANNER_ATTACKER                             (206, 62, 62),
+        BANNER_DEFENDER                             (59, 110, 245),
+        BANNER_BACKGROUND                           (47, 47, 67),
+        BANNER_BORDER                               (0, 0, 0, 120),
 
 
-        BUTTON_MAIN_HOVER                    (255, 93, 104),
-        BUTTON_MAIN_PRESSED                  (80, 80, 96),
-        BUTTON_MAIN_RELEASED                 (64, 64, 96),
-        BUTTON_MAIN_BACKGROUND               (64, 64, 96);
+        BUTTON_OPTION_HOVER                         (255, 93, 104),
+        BUTTON_OPTION_PRESSED                       (80, 80, 96),
+        BUTTON_OPTION_RELEASED                      (64, 64, 96),
+        BUTTON_OPTION_BACKGROUND                    (64, 64, 96),
+
+
+        BUTTON_MAIN_HOVER                           (255, 93, 104),
+        BUTTON_MAIN_PRESSED                         (80, 80, 96),
+        BUTTON_MAIN_RELEASED                        (64, 64, 96),
+        BUTTON_MAIN_BACKGROUND                      (64, 64, 96);
 
 
         private final Color color;
