b4c712b4362b7674661a002e5f1daf89f38d2251
rayzsyt <alihan290404@gmail.com>
2026-03-10 02:30:37 +0100

    Add

---
+++ vit-launcher/src/main/java/de/rayzs/vit/launch/EventManagerImpl.java
@@ -0,0 +1,41 @@
+package de.rayzs.vit.launch;
+
+import de.rayzs.vit.api.addon.Addon;
+import de.rayzs.vit.api.addon.event.Event;
+import de.rayzs.vit.api.addon.event.EventManager;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class EventManagerImpl implements EventManager {
+
+    private final Map<Addon, Set<Event>> events = new HashMap<>();
+
+    @Override
+    public void register(final Addon addon, final Event event) {
+        events.computeIfAbsent(addon, k -> new HashSet<>()).add(event);
+    }
+
+    @Override
+    public void unregisterAll() {
+        events.clear();
+    }
+
+    @Override
+    public void unregisterAll(final Addon addon) {
+        events.remove(addon);
+    }
+
+    @Override
+    public <E extends Event> E call(final E event) {
+        for (final Set<Event> events : events.values()) {
+            for (final Event e : events) {
+                e.call(event);
+            }
+        }
+
+        return event;
+    }
+}
