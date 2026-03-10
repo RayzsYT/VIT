cf173703ed76836bf998287f06d5fb9906ac3fb5
rayzsyt <alihan290404@gmail.com>
2026-03-10 03:34:04 +0100

    Removed body

--- vit-api/src/main/java/de/rayzs/vit/api/event/Event.java
+++ vit-api/src/main/java/de/rayzs/vit/api/event/Event.java
@@ -1,15 +1,3 @@
 package de.rayzs.vit.api.event;
 
-public class Event {
-
-    /**
-     * Default call method.
-     * When creating an addon, just override
-     * this to implement your own call.
-     *
-     * @param event Event.
-     * @return Modified event.
-     */
-    public <E extends Event> E call(final E event) { return event; }
-
-}
+public class Event { }
