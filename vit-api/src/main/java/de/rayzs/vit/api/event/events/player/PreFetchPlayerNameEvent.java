4328e3ebc8096f0224eca7d87521965cc8960c57
rayzsyt <alihan290404@gmail.com>
2026-03-13 00:15:48 +0100

    Changed purpose

--- vit-api/src/main/java/de/rayzs/vit/api/event/events/player/PreFetchPlayerNameEvent.java
+++ vit-api/src/main/java/de/rayzs/vit/api/event/events/player/PreFetchPlayerNameEvent.java
@@ -6,22 +6,19 @@ import de.rayzs.vit.api.event.Event;
  * This event is called before asking the VALORANT servers
  * what the name of a user is.
  */
-public class PrePlayerRegisterEvent extends Event {
+public class PreFetchPlayerNameEvent extends Event {
 
     private final String playerId;
 
     private boolean incognito;
-    private boolean hideLevels;
 
-    public PrePlayerRegisterEvent(
+    public PreFetchPlayerNameEvent(
             final String playerId,
-            final boolean incognito,
-            final boolean hideLevels
+            final boolean incognito
     ) {
         this.playerId = playerId;
 
         this.incognito = incognito;
-        this.hideLevels = hideLevels;
     }
 
     /**
@@ -32,16 +29,6 @@ public class PrePlayerRegisterEvent extends Event {
      */
     public void setIncognito(boolean incognito) {
         this.incognito = incognito;
-    }
-
-    /**
-     * Set visibility of player level.
-     * If enabled, then the levels of the player will be set to -1.
-     *
-     * @param hideLevels IF player level is hidden.
-     */
-    public void setHideLevels(boolean hideLevels) {
-        this.hideLevels = hideLevels;
     }
 
     /**
@@ -60,14 +47,5 @@ public class PrePlayerRegisterEvent extends Event {
      */
     public boolean isIncognito() {
         return this.incognito;
-    }
-
-    /**
-     * If levels are hidden.
-     *
-     * @return True if levels are hidden for the player. False otherwise.
-     */
-    public boolean isHideLevels() {
-        return this.hideLevels;
     }
 }
