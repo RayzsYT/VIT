6accb56a9bf8672de0c21b89950a0db314e9e96a
rayzsyt <alihan290404@gmail.com>
2026-03-12 21:02:33 +0100

    Removed unused imports

--- vit-launcher/src/main/java/de/rayzs/vit/launch/processes/prepare/AssetPreparer.java
+++ vit-launcher/src/main/java/de/rayzs/vit/launch/processes/prepare/AssetPreparer.java
@@ -21,12 +21,9 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.util.*;
 
 public class AssetPreparer {
