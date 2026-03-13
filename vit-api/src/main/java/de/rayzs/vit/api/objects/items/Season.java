f23d3a121a05e5d3458aba0da7034417276fbca0
rayzsyt <alihan290404@gmail.com>
2026-03-13 21:45:26 +0100

    Add active seasons

--- vit-api/src/main/java/de/rayzs/vit/api/objects/items/Season.java
+++ vit-api/src/main/java/de/rayzs/vit/api/objects/items/Season.java
@@ -23,6 +23,8 @@ public record Season(
     // season, is active
     private static final HashMap<Season, Boolean> SEASON_ACTIVITY = new HashMap<>();
 
+    private static Season ACTIVE_SEASON_EPISODE = null, ACTIVE_SEASON_ACT = null;
+
 
     /**
      * Get a season by its id.
@@ -45,6 +47,13 @@ public record Season(
             throw new IllegalArgumentException("Season (" + season.name() + ") is already active!");
         }
 
+        System.out.println("Season: " + season.name + ": ");
+
+        switch (season.type) {
+            case ACT -> ACTIVE_SEASON_ACT = season;
+            case EPISODE -> ACTIVE_SEASON_EPISODE = season;
+        }
+
         SEASON_ACTIVITY.put(season, true);
     }
 
@@ -57,4 +66,21 @@ public record Season(
         return SEASON_ACTIVITY.keySet();
     }
 
+    /**
+     * Get active season act.
+     *
+     * @return Active season act.
+     */
+    public static Season getActiveAct() {
+        return ACTIVE_SEASON_ACT;
+    }
+
+    /**
+     * Get active season episode.
+     *
+     * @return Active season episode.
+     */
+    public static Season getActiveEpisode() {
+        return ACTIVE_SEASON_EPISODE;
+    }
 }
