36b0918c0e03d35255a32e30802897057121df4c
rayzsyt <alihan290404@gmail.com>
2026-03-13 04:30:31 +0100

    Small change

--- vit-addon-predict-map/src/main/java/de/rayzs/vit/mp/MapPredictAddon.java
+++ vit-addon-predict-map/src/main/java/de/rayzs/vit/mp/MapPredictAddon.java
@@ -5,6 +5,8 @@ import de.rayzs.vit.api.addon.Addon;
 import de.rayzs.vit.api.addon.AddonDescription;
 import de.rayzs.vit.api.event.EventAdapter;
 import de.rayzs.vit.api.event.events.game.PreGameInitializeEvent;
+import de.rayzs.vit.api.event.events.gui.InitializeMainGuiEvent;
+import de.rayzs.vit.api.gui.MainGUI;
 
 public class MapPredictAddon extends Addon {
 
@@ -12,14 +14,25 @@ public class MapPredictAddon extends Addon {
         super(api, description);
     }
 
+    private static MainGUI mainGUI;
+
     @Override
     public void onEnable() {
+
+        api.getEventManager().register(this, new EventAdapter<>(InitializeMainGuiEvent.class) {
+
+            @Override
+            public void call(InitializeMainGuiEvent event) {
+                mainGUI = event.getGui();
+            }
+        });
 
         api.getEventManager().register(this, new EventAdapter<>(PreGameInitializeEvent.class) {
 
             @Override
             public void call(PreGameInitializeEvent event) {
-                System.out.println("Map: " + event.getMap().mapName());
+                System.out.println("Found map: " + event.getMap().mapName());
+                mainGUI.setTitle("Predicted Map: " + event.getMap().mapName());
             }
         });
 
