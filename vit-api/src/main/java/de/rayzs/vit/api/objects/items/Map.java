d507e66c31cdd473a1bfab17b036f52a218f8704
rayzsyt <alihan290404@gmail.com>
2026-03-13 04:08:36 +0100

    Add

---
+++ vit-api/src/main/java/de/rayzs/vit/api/objects/items/Map.java
@@ -0,0 +1,64 @@
+package de.rayzs.vit.api.objects.items;
+
+import java.util.HashMap;
+
+public record Map(
+        String mapName,
+        String mapUrl,
+        String mapId
+) {
+
+    private final static HashMap<String, Map> MAP_NAMES = new HashMap<>();
+    private final static HashMap<String, Map> MAP_URLS = new HashMap<>();
+    private final static HashMap<String, Map> MAP_IDS = new HashMap<>();
+
+
+    /**
+     * Registers a map.
+     *
+     * @param mapId Map id.
+     * @param mapUrl Map url.
+     * @param mapName Map name.
+     */
+    public static void loadMap(
+            final String mapId,
+            final String mapUrl,
+            final String mapName
+    ) {
+        final Map map = new Map(mapId, mapUrl, mapName);
+
+        MAP_NAMES.putIfAbsent(mapName, map);
+        MAP_URLS.putIfAbsent(mapUrl, map);
+        MAP_IDS.putIfAbsent(mapId, map);
+    }
+
+    /**
+     * Get map by map id.
+     * @param mapId Map id.
+     *
+     * @return Map if found. NULL otherwise.
+     */
+    public static Map getMapById(final String mapId) {
+        return MAP_IDS.get(mapId);
+    }
+
+    /**
+     * Get map by map url.
+     * @param mapUrl Map url.
+     *
+     * @return Map if found. NULL otherwise.
+     */
+    public static Map getMapByUrl(final String mapUrl) {
+        return MAP_URLS.get(mapUrl);
+    }
+
+    /**
+     * Get map by map name.
+     * @param mapName Map name.
+     *
+     * @return Map if found. NULL otherwise.
+     */
+    public static Map getMapByName(final String mapName) {
+        return MAP_NAMES.get(mapName);
+    }
+}
