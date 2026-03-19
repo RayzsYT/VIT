b2596e3b2a7f5ff3e983e078d068ef5ccc2eefd0
rayzsyt <alihan290404@gmail.com>
2026-03-19 04:47:14 +0100

    Add team id to simplify work

--- vit-api/src/main/java/de/rayzs/vit/api/objects/items/Team.java
+++ vit-api/src/main/java/de/rayzs/vit/api/objects/items/Team.java
@@ -2,14 +2,15 @@ package de.rayzs.vit.api.objects.items;
 
 public enum Team {
 
-    DEFEND      ("Defender", "defending"),
-    ATTACK      ("Attacker", "attacking");
+    DEFEND      ("Defender", "blue",    "defending"),
+    ATTACK      ("Attacker", "red",     "attacking");
 
 
-    private final String teamName, teamAdjective;
+    private final String teamName, teamId, teamAdjective;
 
-    Team(final String teamName, final String teamAdjective) {
+    Team(final String teamName, final String teamId, final String teamAdjective) {
         this.teamName = teamName;
+        this.teamId = teamId;
         this.teamAdjective = teamAdjective;
     }
 
@@ -23,6 +24,15 @@ public enum Team {
     }
 
     /**
+     * Get team id based on how the VALORANT api calls it.
+     *
+     * @return Team id.
+     */
+    public String getTeamId() {
+        return this.teamId;
+    }
+
+    /**
      * Returns the team adjective.
      * Example: defending, attacking
      *
@@ -32,17 +42,38 @@ public enum Team {
         return this.teamAdjective;
     }
 
+
+
     /**
      * Iterates through all teams and compare their names
      * with the provided input. Returns the team if it finds a match,
      * otherwise returns null.
      *
      * @param teamName Team name. Letter casing does not matter!
-     * @return Returns matched agent or null.
+     * @return Returns matched team or null.
      */
     public static Team getTeamByName(final String teamName) {
         for (final Team team : Team.values()) {
             if (team.getTeamName().equalsIgnoreCase(teamName)) {
+                return team;
+            }
+        }
+
+        return null;
+    }
+
+
+    /**
+     * Iterates through all teams and compare their ids
+     * with the provided input. Returns the team if it finds a match,
+     * otherwise returns null.
+     *
+     * @param teamId Team id. Letter casing does not matter!
+     * @return Returns matched team or null.
+     */
+    public static Team getTeamById(final String teamId) {
+        for (final Team team : Team.values()) {
+            if (team.getTeamId().equalsIgnoreCase(teamId)) {
                 return team;
             }
         }
