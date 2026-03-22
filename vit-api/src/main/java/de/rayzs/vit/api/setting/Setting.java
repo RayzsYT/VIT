06cb1df2568ec54d6643e5f8d97b095021a5bc19
rayzsyt <alihan290404@gmail.com>
2026-03-22 05:43:37 +0100

    Add for later groundwork

---
+++ vit-api/src/main/java/de/rayzs/vit/api/setting/Setting.java
@@ -0,0 +1,6 @@
+package de.rayzs.vit.api.setting;
+
+public class Setting {
+
+
+}
---
+++ vit-api/src/main/java/de/rayzs/vit/api/setting/SettingType.java
@@ -0,0 +1,5 @@
+package de.rayzs.vit.api.setting;
+
+public enum SettingType {
+    CHECK_BOX, TEXT_AREA, NUMBER_AREA
+}
