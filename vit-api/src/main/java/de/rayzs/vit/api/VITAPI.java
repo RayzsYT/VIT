9873947b8b3d5895e262cb79ef0785cc0a5d0d47
rayzsyt <alihan290404@gmail.com>
2026-08-22 06:02:00 +0200

    Update version

--- vit-api/src/main/java/de/rayzs/vit/api/VITAPI.java
+++ vit-api/src/main/java/de/rayzs/vit/api/VITAPI.java
@@ -26,7 +26,7 @@ public interface VITAPI {
      *
      * @return VIT version.
      */
-    static String getVersion() { return "1.0.14"; }
+    static String getVersion() { return "1.0.15"; }
 
     /**
      * Get current screen.
