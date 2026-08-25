459af888990ffff0d39d142a9db4757ec1a1c6d1
rayzsyt <alihan290404@gmail.com>
2026-08-25 05:14:55 +0200

    Small changes

--- vit-launcher/src/main/resources/scripts/start.bat
+++ vit-launcher/src/main/resources/scripts/start.bat
@@ -19,12 +19,12 @@ cls
 :: Preparing java (Not actual installer though. Just a smaller copy of it inside the VIT folder.)
 if not exist "%JAVA_SANITY_CHECK%" (
 
-    echo Alright, here we go! Let's prepare Java first.
+    echo So, let's gets started. First, we gotta install Java. This will take a while...
     title Preparing Java...
 
     if not exist "%JAVA_DIR%" mkdir "%JAVA_DIR%"
 
-    echo Downloading...
+    echo Let's start downloading! :D
     curl -L -o "%TMP_JAVA_ZIP_FILE%" "%JAVA_URL%"
 
     if errorlevel 1 (
@@ -32,7 +32,7 @@ if not exist "%JAVA_SANITY_CHECK%" (
         exit /b 1
     )
 
-    echo Unzipping...
+    echo Done. Now let's unzip it...
     powershell -NoProfile -Command "Expand-Archive -LiteralPath '%TMP_JAVA_ZIP_FILE%' -DestinationPath '%JAVA_DIR%' -Force"
 
     if errorlevel 1 (
@@ -40,16 +40,14 @@ if not exist "%JAVA_SANITY_CHECK%" (
         exit /b 1
     )
 
-    echo Sweeping all the sweat after all the hard work...
     del /q "%TMP_JAVA_ZIP_FILE%"
-
     echo Done!
 )
 
 :: Downloading and starting VIT now.
 if not exist "%VIT_JAR_FILE%" (
 
-    echo Downloading latest version of VIT...
+    echo Let's download the latest version of VIT...
     title Preparing VIT installation setup...
 
     powershell -NoProfile -Command "$release = Invoke-RestMethod -Uri '%GITHUB_URL%'; $jar = $release.assets | Where-Object { $_.name -like '*.jar' } | Select-Object -First 1; if (-not $jar) { exit 1 }; Invoke-WebRequest -Uri $jar.browser_download_url -OutFile '%VIT_JAR_FILE%'"
@@ -63,7 +61,7 @@ if not exist "%VIT_JAR_FILE%" (
 )
 
 
-:: Starting VIT
+:: Check if VIT has an updated version available for use.
 cd /d %VIT_DIR%
 if exist "latest-updated.jar" (
     echo Found latest-updated.jar, updating...
@@ -75,4 +73,5 @@ if exist "latest-updated.jar" (
     ren "latest-updated.jar" "latest.jar"
 )
 
+:: Start VIT.
 start "" "java/jdk21.0.3-windows-x64/java-windows/bin/javaw.exe" -jar latest.jar
