f3f75f38e48c9b6827fd5683712039771cba6f19
rayzsyt <alihan290404@gmail.com>
2026-08-25 19:16:50 +0200

    Embedded option to decide whether to save a match or not.

--- vit-api/src/main/java/de/rayzs/vit/api/event/events/game/match/GameMatchStartEvent.java
+++ vit-api/src/main/java/de/rayzs/vit/api/event/events/game/match/GameMatchStartEvent.java
@@ -6,15 +6,36 @@ import de.rayzs.vit.api.objects.game.Game;
 
 /**
  * Called after {@link GameInitializedEvent} once the actual match
- * started. Only when the {@link de.rayzs.vit.api.session.SessionState}
+ * started. This event also decides whether a game should be saved or not.
+ * Only called when the {@link de.rayzs.vit.api.session.SessionState}
  * is set to {@link de.rayzs.vit.api.session.SessionState#IN_GAME}.
  */
 public class GameMatchStartEvent extends Event {
 
     private final Game game;
+    private boolean saveGame;
 
-    public GameMatchStartEvent(final Game game) {
+    public GameMatchStartEvent(final Game game, final boolean saveGame) {
         this.game = game;
+        this.saveGame = saveGame;
+    }
+
+    /**
+     * Should VIT save the game?
+     *
+     * @return Boolean indicating whether to save the game or not.
+     */
+    public boolean doesSaveGame() {
+        return this.saveGame;
+    }
+
+    /**
+     * Set whether game should be saved or not.
+     *
+     * @param saveGame Boolean whether the game should be saved or not.
+     */
+    public void setSaveGame(final boolean saveGame) {
+        this.saveGame = saveGame;
     }
 
     /**
